cefa14414f891f3cd6c301aee3af5f7c222494cb
Adam <adam.j.mccor@gmail.com>
2021-06-04 16:46:59 +0100

    'add statement spec and pass'

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,13 +1,17 @@
 class BankAccount
     DEFAULT = 0
+    GA_DEFAULT = 0
     attr_accessor :balance
 
-    def initialize(balance=DEFAULT)
+    def initialize(balance=DEFAULT, given_amount=DEFAULT)
         @balance = balance
+        @given_amount = given_amount
     end
 
     def deposit(amount)
-        @balance += amount
+        @given_amount += amount
+        @balance += @given_amount
+        @given_amount = 0
     end
 
     def withdraw(amount)
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,19 +4,36 @@ require 'statement'
 
 describe Statement do
 
-    describe '#date_save' do
-        it 'saves the current date during a transaction' do
+    describe '#date_record' do
+        it 'records the current date for the transaction' do
             account = BankAccount.new
             statement = Statement.new
 
             account.deposit(100)
 
-            expect(statement.date).to eq(Time.now.strftime("%d/%m/%Y"))
+            expect(statement.date_record).to eq(Time.now.strftime("%d/%m/%Y"))
+        end
+    end
+
+    describe '#amount_record' do
+        it 'records the amount given for the transaction' do
+            account = BankAccount.new
+            statement = Statement.new
+
+            account.deposit(100)
+
+            expect(statement.amount_record).to eq(account.given_amount)
         end
     end
 
 
-
-
+    describe '#balance_record' do
+        it 'records the current balance after the transaction' do
+            account = BankAccount.new
+            account.deposit(100)
+            
+            expect(statement.balance_record).to eq(account.balance)
+        end
+    end
 
 end
